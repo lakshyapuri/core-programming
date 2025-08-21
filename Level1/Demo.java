465a85009af9300da08b697a0d197e5b94ddeb05
lakshyapuri <lakshyapuri2004@gmail.com>
2025-08-21 12:27:33 +0530

    Removed the extra file and created Level 1 folder with first problem

---
+++ Level1/Demo.java
@@ -0,0 +1,9 @@
+
+public class Demo{
+ public static void main(String args[]){
+ int birthYear =   2000;
+ int currentYear = 2024;
+ int finalAge = currentYear-birthYear;
+ System.out.println("harry age in 2024 is: "+" "+finalAge);
+ }
+}
